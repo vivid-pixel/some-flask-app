5b68ddff983eb6208c7b95d2f5e7dc77f0c4a664
vivid-pixel <42875253+vivid-pixel@users.noreply.github.com>
2024-03-28 12:16:03 -0700

    the basic framework for a python flask app

---
+++ app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from flask import Flask, redirect, url_for, request, render_template, session
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
